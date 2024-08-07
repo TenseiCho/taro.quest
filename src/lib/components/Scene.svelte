2e745b445e2eed60df24c5baab9b8939edd88c18
Jay Garner <zellasphere@gmail.com>
2024-08-06 20:06:31 -0400

    finally

--- src/lib/components/Scene.svelte
+++ src/lib/components/Scene.svelte
@@ -1,29 +1,7 @@
-<!-- <script>
-    import { T } from '@threlte/core';
-    import { OrbitControls, Grid } from '@threlte/extras';
-    import JpGirl from './models/jp-girl.svelte';
-</script>
-
-<T.PerspectiveCamera makeDefault position={[0, 5, 10]} fov={45}>
-    <OrbitControls enableDamping />
-</T.PerspectiveCamera>
-
-<T.DirectionalLight intensity={2} position={[5, 5, 5]} />
-<T.AmbientLight intensity={1} />
-
-
-
-
-<Grid />
-
-<jp-girl />
- -->
-
-
 <script>
 	import { T } from '@threlte/core';
 	import { OrbitControls, Grid } from '@threlte/extras';
-	import Girl from './models/girl.svelte';
+	import Girl from './models/jp-girl.svelte';
 </script>
 
 <T.PerspectiveCamera makeDefault position={[0, 5, 10]} fov={45}>
